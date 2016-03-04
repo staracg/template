51303fd4029ab285c5f60590f76d024275eda930
Ayumi <staracg@gmail.com>
2016-03-04 17:51:42 +0800

    Create pwn.py

---
+++ pwn.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from pwn import *
+info = log.info
+
+# setting 
+elf = ELF("")
+
+def local():
+	global libc, r
+	libc = ELF("local")
+	r = remote("localhost", 5566)
+
+def fuck():
+	global libc, r
+	libc = ELF("libc.so.6")
+	r = remote("52.68.53.28", 56746)
+
+local()
+
+r.interactive()
